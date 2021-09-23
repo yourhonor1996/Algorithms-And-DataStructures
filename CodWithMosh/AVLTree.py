b3e9db97ac545b3b98e2b87d1c5865a620ab3f8b
yourhonor1996 <mohammad.alavi.74@gmail.com>
2021-09-23 23:05:41 +0330

    complete avl tree

--- CodWithMosh/AVLTree.py
+++ CodWithMosh/AVLTree.py
@@ -1,17 +1,16 @@
 from dataclasses import dataclass
 
 
-class AVLTree(object):
+class AVL(object):
     default = "default"
     left = "left"
     right = "right"
 
     class Node(object):
         def __init__(self, data) -> None:
-
             self.data = data
-            self.left: AVLTree.Node = None
-            self.right: AVLTree.Node = None
+            self.left: AVL.Node = None
+            self.right: AVL.Node = None
             self.height: int = 0
 
         def __repr__(self) -> str:
@@ -19,79 +18,93 @@ class AVLTree(object):
 
         @property
         def is_leftheavy(self):
-            return AVLTree.balance_factor(self) > 1
+            return AVL.balance_factor(self) > 1
 
         @property
         def is_rightheavy(self):
-            return AVLTree.balance_factor(self) < -1
+            return AVL.balance_factor(self) < -1
 
     def __init__(self, root_data=None) -> None:
-        self.root = AVLTree.Node(root_data)
+        self.root = AVL.Node(root_data)
 
     @staticmethod
-    def get_height(node: "AVLTree.Node"):
+    def get_height(node: "AVL.Node"):
         return node.height if node else -1
 
     @staticmethod
-    def balance_factor(node: "AVLTree.Node"):
-        return AVLTree.get_height(node.left) - AVLTree.get_height(node.right) if node else 0
+    def balance_factor(node: "AVL.Node"):
+        return AVL.get_height(node.left) - AVL.get_height(node.right) if node else 0
 
     def insert(self, data):
         self.root = self.__insert(data)
 
-    def __insert(self, data, root: "AVLTree.Node" = "default"):
+    def __insert(self, data, root: "AVL.Node" = "default"):
 
-        if root == AVLTree.default:
+        if root == AVL.default:
             root = self.root
 
         if root is None or root.data is None:
-            return AVLTree.Node(data)
+            return AVL.Node(data)
 
         if data < root.data:
             root.left = self.__insert(data, root.left)
         else:
             root.right = self.__insert(data, root.right)
 
-        root.height = max(AVLTree.get_height(root.left), AVLTree.get_height(root.right)) + 1
+        AVL.reset_height(root)
 
-        if root.is_rightheavy:
-            left_bf = AVLTree.balance_factor(root.right)
-            if left_bf > 0:
-                # TODO make rotations here
-                print(f"right rotation {root.right}")
-            print(f"left rotation {root}")
-
-        elif root.is_leftheavy:
-            right_bf = AVLTree.balance_factor(root.left)
-            if right_bf < 0:
-                # TODO make rotations here
-                print(f"left rotation {root.left}")
-            print(f"right rotation {root}")
+        root = AVL.balance(root)
 
         return root
 
-    # TODO create this method
-    # def rotate(self, direction: str, root: "AVLTree.Node"):
-    #     if direction == AVLTree.left:
-    #         if root.right.left:
-    #             root.left = root.right.left
-    #         newroot = root.right
-    #         newroot.left = root
+    @staticmethod
+    def rotate(direction: str, root: "AVL.Node"):
+        if direction == AVL.left:
+            newroot = root.right
+            root.right = newroot.left
+            newroot.left = root
 
-    #     elif direction == AVLTree.right:
-    #         if root.left.right:
-    #             root.right = root.left.right
-    #         newroot = root.left
-    #         newroot.right = root
+        elif direction == AVL.right:
+            newroot = root.left
+            root.left = newroot.right
+            newroot.right = root
 
-    #     return newroot
+        AVL.reset_height(root)
+        AVL.reset_height(newroot)
+
+        return newroot
+
+    @staticmethod
+    def reset_height(node: "AVL.Node"):
+        node.height = max(AVL.get_height(node.left), AVL.get_height(node.right)) + 1
+
+    @staticmethod
+    def balance(root: "AVL.Node"):
+        if root.is_leftheavy:
+            if AVL.balance_factor(root.left) < 0:
+                root.left = AVL.rotate(AVL.left, root.left)
+            return AVL.rotate(AVL.right, root)
+
+        elif root.is_rightheavy:
+            if AVL.balance_factor(root.right) > 0:
+                root.right = AVL.rotate(AVL.right, root.right)
+            return AVL.rotate(AVL.left, root)
+
+        return root
 
 
-avl = AVLTree()
-avl.insert(30)
-avl.insert(20)
+avl = AVL()
 avl.insert(10)
+avl.insert(20)
+avl.insert(30)
+avl.insert(15)
+avl.insert(40)
+avl.insert(9)
+avl.insert(50)
+avl.insert(60)
+avl.insert(70)
 avl.insert(25)
+avl.insert(100)
 
-avl.root = avl.rotate(AVLTree.right, avl.root)
+# avl.root = avl.rotate(AVL.right, avl.root)
 print("done")
