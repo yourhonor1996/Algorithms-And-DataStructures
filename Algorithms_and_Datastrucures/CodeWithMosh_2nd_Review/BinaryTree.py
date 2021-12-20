d7dad608364b39b432c1a81d7e3b4f8222690994
yourhonor1996 <mohammad.alavi.74@gmail.com>
2021-12-20 22:05:29 +0330

    [ADD] depth first traversals

--- Algorithms_and_Datastrucures/CodeWithMosh_2nd_Review/BinaryTree.py
+++ Algorithms_and_Datastrucures/CodeWithMosh_2nd_Review/BinaryTree.py
@@ -13,6 +13,10 @@ class Node:
     def __repr__(self) -> str:
         return f"Node<{self.value}>"
 
+    @property
+    def has_no_childern(self):
+        return (self.right is None) and (self.left is None)
+
 
 class BinaryTree:
     def __init__(self, root_value=None) -> None:
@@ -53,17 +57,56 @@ class BinaryTree:
             else:
                 current = current.right
 
+    def traverse_executer(self, traverse_function, root_node: Node = None):
+        if root_node is None:
+            root_node = self.root
+        results = []
+        traverse_function(root_node, results)
+        return results
+
+    def traverse_pre_order(self, root: Node, results: list):
+        if root is None:
+            return
+
+        results.append(root)
+        self.traverse_pre_order(root.left, results)
+        self.traverse_pre_order(root.right, results)
+
+    def traverse_in_order(self, root: Node, results: list):
+        if root is None:
+            return
+
+        self.traverse_in_order(root.left, results)
+        results.append(root)
+        self.traverse_in_order(root.right, results)
+
+    def traverse_post_order(self, root:Node, results:list):
+        if root is None:
+            return
+
+        self.traverse_post_order(root.left, results)
+        self.traverse_post_order(root.right, results)
+        results.append(root)
+
+    def do_traverse_pre_order(self):
+        return self.traverse_executer(self.traverse_pre_order)
+
+    def do_traverse_in_order(self):
+        return self.traverse_executer(self.traverse_in_order)
+
+    def do_traverse_post_order(self):
+        return self.traverse_executer(self.traverse_post_order)
+
 
 tree = BinaryTree()
-tree.insert(10)
-tree.insert(2)
-tree.insert(3)
-tree.insert(13)
-tree.insert(15)
-tree.insert(65)
+tree.insert(7)
+tree.insert(4)
+tree.insert(9)
+tree.insert(1)
 tree.insert(6)
+tree.insert(8)
+tree.insert(10)
 print('sdf')
-print(tree.find(8))
-print(tree.find(15))
-print(tree.find(699))
-print(tree.find(6))
+print(tree.do_traverse_pre_order())
+print(tree.do_traverse_in_order())
+print(tree.do_traverse_post_order())
